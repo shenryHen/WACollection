27130babf522fa0cd25ed49d266afae9b802d79b
mlh-henry <henry.shen@outlook.com>
2020-10-17 14:22:47 -0400

    front end assets

--- download-display/app.py
+++ download-display/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template	
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 import datetime
 import threading
@@ -8,22 +8,29 @@ from google.cloud import firestore
 
 # Project ID is determined by the GCLOUD_PROJECT environment variable
 db = firestore.Client() 
+users_ref = db.collection(u'downloads').document('Bronny James').get()
+print(users_ref.to_dict())
 
-#users_ref = db.collection(u'downloads')
-# docs = users_ref.stream()
+callback_done = threading.Event()
 
-# l = []
-# for doc in docs:
-#     l.append(doc.to_dict())
+def get_downloads():
+    while False:
+        print("listening to doc updates")
+        def on_snapshot(col_snapshot, changes, read_time):
+            for doc in col_snapshot:
+                user_dict = doc.to_dict()
+                temp = user_dict['user']
+                print(f'Received document snapshot: {temp}')
+            callback_done.set()
+        col_query = db.collection(u'downloads')
+        query_watch = col_query.on_snapshot(on_snapshot)
+        sleep(1)
 
-def on_snapshot(doc_snapshot, changes, read_time):
-    for doc in doc_snapshot:
-        print(f'Received document snapshot: {doc.user}')
-    callback_done.set()
-users_ref = db.collection(u'downloads')
+@app.route('/_downloads')
+def hello_world():
+    users_ref = db.collection(u'downloads').document('Bronny James').get()
+    return jsonify(users_ref.to_dict())
 
-doc_watch = users_ref.on_snapshot(on_snapshot)
-
-# @app.route('/')
-# def hello_world():
-#     return render_template('downloads.html', users=l)
+@app.route('/')
+def downloads():
+    return render_template('layouts/downloads.html', users=None)
---
+++ download-display/templates/static/js/app.js
@@ -0,0 +1,5 @@
+$.get("localhost:5000", function(data){
+	$(".result").html(data){
+		console.log("good stuff happened")
+	}
+})
